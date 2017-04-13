01813c9746f960a33a1c141356fa38b30bac5501
TonyLiu <zhendong.nus@gmail.com>
2017-04-13 09:42:28 +0800

    assignment4 4mb driver

---
+++ assignment4/one.c
@@ -0,0 +1,99 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#define MAJOR_NUMBER 61
+ 
+size_t length = 1;
+/* forward declaration */
+int onebyte_open(struct inode *inode, struct file *filep);
+int onebyte_release(struct inode *inode, struct file *filep);
+ssize_t onebyte_read(struct file *filep, char *buf, size_t
+count, loff_t *f_pos);
+ssize_t onebyte_write(struct file *filep, const char *buf,
+size_t count, loff_t *f_pos);
+static void onebyte_exit(void);
+/* definition of file_operation structure */
+struct file_operations onebyte_fops = {
+     read:     onebyte_read,
+     write:    onebyte_write,
+     open:     onebyte_open,
+     release: onebyte_release
+};
+char *onebyte_data = NULL;
+int onebyte_open(struct inode *inode, struct file *filep)
+{
+     return 0; // always successful
+}
+int onebyte_release(struct inode *inode, struct file *filep)
+{
+     return 0; // always successful
+}
+ssize_t onebyte_read(struct file *filep, char *buf, size_t
+count, loff_t *f_pos)
+{
+    printk(KERN_ALERT "Read count = %lu\n", count);
+    if (onebyte_data != NULL && count >= 1 && 0 == *f_pos) {
+        copy_to_user(buf, onebyte_data, length);
+        (*f_pos)++;
+    } else {
+        return 0;
+    }
+    return length;
+}
+ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
+{
+    printk(KERN_ALERT "Write count = %lu\n", count);
+    if (buf != NULL && count >= 1 && 0 == *f_pos) {
+        copy_from_user(onebyte_data, buf, count);
+        (*f_pos) += count;
+    } else {
+        return -ENOSPC;
+    }
+    length = count;
+    return count;
+}
+static int onebyte_init(void)
+{
+     int result;
+     // register the device
+     result = register_chrdev(MAJOR_NUMBER, "one",
+&onebyte_fops);
+     if (result < 0) {
+         return result;
+     }
+     // allocate one byte of memory for storage
+     // kmalloc is just like malloc, the second parameter is
+// the type of memory to be allocated.
+     // To release the memory allocated by kmalloc, use kfree.
+     onebyte_data = kmalloc(sizeof(char) * 1024 * 1024 * 4, GFP_KERNEL);
+     if (!onebyte_data) {
+          onebyte_exit();
+          // cannot allocate memory
+          // return no memory error, negative signify a failure
+         return -ENOMEM;
+     }        
+     // initialize the value to be X
+     *onebyte_data = 'X';
+     printk(KERN_ALERT "This is a onebyte device module\n");
+     return 0;
+}
+static void onebyte_exit(void)
+{
+     // if the pointer is pointing to something
+     if (onebyte_data) {
+          // free the memory and assign the pointer to NULL
+          kfree(onebyte_data);
+          onebyte_data = NULL;
+     }         // unregister the device
+     unregister_chrdev(MAJOR_NUMBER, "one");
+     printk(KERN_ALERT "Onebyte device module is unloaded\n");
+}
+MODULE_LICENSE("GPL");
+module_init(onebyte_init);
+module_exit(onebyte_exit);
